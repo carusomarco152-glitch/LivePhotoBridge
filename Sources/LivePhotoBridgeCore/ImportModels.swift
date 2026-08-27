494be5f2d1e8eee26e8e234fc81386cb9c229f40
carusomarco152-glitch <carusomarco152@gmail.com>
2026-08-27 23:52:16 +0200

    Unify persistent import summary model

--- Sources/LivePhotoBridgeCore/ImportModels.swift
+++ Sources/LivePhotoBridgeCore/ImportModels.swift
@@ -22,28 +22,34 @@ public struct ImportItem: Identifiable, Codable, Sendable {
 }
 
 public struct ImportSummary: Codable, Sendable {
-    public let startedAt: Date
+    public var startedAt: Date
     public var finishedAt: Date?
     public var totalFiles: Int
+    public var completedFiles: Int
+    public var failedFiles: Int
+    public var skippedFiles: Int
     public var photos: Int
     public var videos: Int
     public var livePhotosRecognized: Int
     public var livePhotosIncomplete: Int
-    public var imported: Int
-    public var skipped: Int
-    public var failed: Int
+    public var bytesTransferred: Int64
 
     public init(startedAt: Date = Date()) {
         self.startedAt = startedAt
         self.finishedAt = nil
         self.totalFiles = 0
+        self.completedFiles = 0
+        self.failedFiles = 0
+        self.skippedFiles = 0
         self.photos = 0
         self.videos = 0
         self.livePhotosRecognized = 0
         self.livePhotosIncomplete = 0
-        self.imported = 0
-        self.skipped = 0
-        self.failed = 0
+        self.bytesTransferred = 0
+    }
+
+    public var progress: Double {
+        totalFiles == 0 ? 0 : Double(completedFiles + failedFiles + skippedFiles) / Double(totalFiles)
     }
 }
 
